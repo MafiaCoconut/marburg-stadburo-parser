441fbe3624339c0ce602ae292156e718ab98b07c
MafiaCoconut <alex.kach.2000@gmail.com>
2024-06-12 12:28:58 +0200

    текст термиинов выводится корректно

--- src/application/use_cases/get_termins_use_case.py
+++ src/application/use_cases/get_termins_use_case.py
@@ -19,16 +19,42 @@ class GetTerminsUseCase:
 
     def get_type(self, category_of_termins: int, locale: str):
         termins = self.termins_repository.get_by_type(category_of_termins)
+        if termins is not None:
 
-        text = self.translation_service.translate(
-            message_id="list-of-all-termins",
-            locale=locale,
-            termins_name=self.category_of_termins_repository.get_name(category_id=category_of_termins),
-            time=termins[0].created_at.strftime("%H:%M"),
-            day_last_activate=termins[0].created_at.strftime("%d.%m.%Y")
-        ) + "\n\n"
+            text = self.translation_service.translate(
+                message_id="list-of-all-termins",
+                locale=locale,
+                termins_name=self.category_of_termins_repository.get_name(category_id=category_of_termins),
+                time=termins[0].created_at.strftime("%H:%M"),
+                day_last_activate=termins[0].created_at.strftime("%d.%m.%Y")
+            ) + "\n\n"
 
-        for termin in termins:
-            pass
+            k = 0
+            for i, termin in enumerate(termins):
+                k += 1
+                if termin.time.day != termins[i-1].time.day:
+                    if text[-1] != '\n':
+                        text += '\n'
+                    text += f"\n<b>{termin.time.strftime('%d.%m.%Y')}<b>\n"
+                    k = 1
+
+                text += f"{termin.time.strftime('%H:%M')} "
+                if k % 6 == 0:
+                    text += '\n'
+
+            print(text)
+
+
+            """
+            day
+            termin1 termin2 termin3 termin4 termin5 termin6 
+            """
+
+        else:
+            text = self.translation_service.translate(
+                message_id="lack-of-terms",
+                locale=locale)
 
         return text
+
+
