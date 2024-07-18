7e16d56111cf2dbe29fff9dad7524b59e4f07b92
MafiaCoconut <alex.kach.2000@gmail.com>
2024-07-18 21:54:42 +0200

    добавил улучшение в api

--- src/infrastructure/web/api.py
+++ src/infrastructure/web/api.py
@@ -15,18 +15,15 @@ def start_parser_all(get_result: bool):
 
 
 @router.get("/parser/{category_of_termin_id}")
-def start_parser(category_of_termin_id: int, get_result: bool = False):
+def start_parser(category_of_termin_id: str, get_result: bool = False):
     if get_result:
-        return termins_service.parse_termins_category(termin_category_id=category_of_termin_id)
+        return termins_service.parse_termins_category(termin_category_id=int(category_of_termin_id))
     else:
-        termins_service.parse_termins_category(termin_category_id=category_of_termin_id)
-
-
-
+        termins_service.parse_termins_category(termin_category_id=int(category_of_termin_id))
 
 
 @router.get("/category_of_termins/{category_of_termins}")
-def get_text_category_of_termins(category_of_termins: int, locale: str):
-    return termins_service.get_text_category_of_termins(category_of_termins=category_of_termins, locale=locale)
+def get_text_category_of_termins(category_of_termins: str, locale: str):
+    return termins_service.get_text_category_of_termins(category_of_termins=int(category_of_termins), locale=locale)
 
 
