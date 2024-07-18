71c1ce858165dbcb1c516a8b33d00937b2e489fd
MafiaCoconut <alex.kach.2000@gmail.com>
2024-07-18 21:29:04 +0200

    добавил тесты для проверки получение терминов

--- tests/test_get_termins_list.py
+++ tests/test_get_termins_list.py
@@ -32,5 +32,28 @@ class TestGetTerminsList:
 
         assert termins == translation_service.translate(message_id="lack-of-terms",locale='en')
 
+    @staticmethod
+    @pytest.mark.parametrize(
+        "termins_list",
+        [
+            "adressanderung",
+            "eat_abholung",
+            "registration_office",
+            "others"
+        ],
+        indirect=True
+    )
+    def test_get_category_of_termins_text(termins_service, translation_service, termins_list):
+        category_of_termins, test_termins = termins_list
+        termins_service.save_termins(test_termins)
+        for language in ['ru', 'en', 'de', 'uk', 'ar']:
+            termins = termins_service.get_text_category_of_termins(
+                category_of_termins=category_of_termins, locale=language
+            )
+
+            assert isinstance(termins, str)
+
+
+
 
 
