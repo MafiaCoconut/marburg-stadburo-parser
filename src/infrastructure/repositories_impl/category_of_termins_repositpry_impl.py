b93f0e25569bd4ff5f5956e1c689f277e3bb27a9
MafiaCoconut <alex.kach.2000@gmail.com>
2024-07-22 18:30:14 +0200

    изменил синхронные сессия к бд на асинхронные

--- src/infrastructure/repositories_impl/category_of_termins_repositpry_impl.py
+++ src/infrastructure/repositories_impl/category_of_termins_repositpry_impl.py
@@ -2,21 +2,21 @@ from sqlalchemy import select, delete, text
 
 from application.repositories.category_of_termins_repository import CategoryOfTerminsRepository
 from domain.entities.category_of_termin import CategoryOfTermins
-from infrastructure.db.base import session_factory
+from infrastructure.db.base import session_factory, async_session_factory
 from infrastructure.db.models.categories_of_termins_orm import CategoriesOfTerminsOrm
 
 
 class CategoryOfTerminsRepositoryImpl(CategoryOfTerminsRepository):
     @staticmethod
     def get_name(category_id: int):
-        with session_factory() as session:
+        with async_session_factory() as session:
             query = (select(CategoriesOfTerminsOrm.name).filter(CategoriesOfTerminsOrm.category_id == category_id))
             result = session.execute(query)
             return result.scalars().all()[0]
 
     @staticmethod
     def save(category_of_termins: CategoryOfTermins):
-        with session_factory() as session:
+        with async_session_factory() as session:
             category_of_termins_orm = CategoriesOfTerminsOrm(
                 category_id=category_of_termins.category_id,
                 name=category_of_termins.name
@@ -26,7 +26,7 @@ class CategoryOfTerminsRepositoryImpl(CategoryOfTerminsRepository):
 
     @staticmethod
     def delete_all():
-        with session_factory() as session:
+        with async_session_factory() as session:
 
             session.execute(delete(CategoriesOfTerminsOrm))
             session.execute(text(f"ALTER SEQUENCE {CategoriesOfTerminsOrm.__tablename__}_category_id_seq RESTART WITH 1;"))
--- src/infrastructure/repositories_impl/termins_repository_impl.py
+++ src/infrastructure/repositories_impl/termins_repository_impl.py
@@ -1,6 +1,6 @@
 from application.repositories.termins_repository import TerminsRepository
 from domain.entities.termin import Termin
-from infrastructure.db.base import sync_engine, session_factory, Base
+from infrastructure.db.base import sync_engine, session_factory, Base, async_session_factory
 from sqlalchemy import select, delete
 
 from infrastructure.db.models.termins_orm import TerminsOrm
@@ -9,14 +9,14 @@ from infrastructure.db.models.termins_orm import TerminsOrm
 class TerminsRepositoryImpl(TerminsRepository):
     @staticmethod
     def get_all() -> list[Termin]:
-        with session_factory() as session:
+        with async_session_factory() as session:
             query = select(TerminsOrm)
             result = session.execute(query)
             return result.scalars().all()
 
     @staticmethod
     def get_by_type(category_id: int) -> list[Termin]:
-        with session_factory() as session:
+        with async_session_factory() as session:
             query = (
                 select(TerminsOrm)
                 .filter(TerminsOrm.category_id == int(category_id))
@@ -27,7 +27,7 @@ class TerminsRepositoryImpl(TerminsRepository):
 
     @staticmethod
     def save(termin: Termin) -> None:
-        with session_factory() as session:
+        with async_session_factory() as session:
             termin_orm = TerminsOrm(
                 category_id=termin.category_id,
                 time=termin.time
@@ -37,7 +37,7 @@ class TerminsRepositoryImpl(TerminsRepository):
 
     @staticmethod
     def save_many(termins: list[Termin]) -> None:
-        with session_factory() as session:
+        with async_session_factory() as session:
             for termin in termins:
                 termin_orm = TerminsOrm(
                     category_id=termin.category_id,
@@ -48,14 +48,14 @@ class TerminsRepositoryImpl(TerminsRepository):
 
     @staticmethod
     def delete_all() -> None:
-        with session_factory() as session:
+        with async_session_factory() as session:
             query = delete(TerminsOrm)
             session.execute(query)
             session.commit()
 
     @staticmethod
     def delete_by_category(category_id: int) -> None:
-        with session_factory() as session:
+        with async_session_factory() as session:
             query = (
                 delete(TerminsOrm)
                 .filter(TerminsOrm.category_id == int(category_id))
@@ -65,7 +65,7 @@ class TerminsRepositoryImpl(TerminsRepository):
 
     @staticmethod
     def delete(termin_id: int) -> None:
-        with session_factory() as session:
+        with async_session_factory() as session:
             query = (
                 delete(TerminsOrm)
                 .filter(TerminsOrm.termin_id == int(termin_id))
