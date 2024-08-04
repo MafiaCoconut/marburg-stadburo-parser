25f6ad771b491526c11116930879104cf031d472
MafiaCoconut <alex.kach.2000@gmail.com>
2024-08-04 22:37:12 +0200

    переделал модели на  pydantic

--- src/domain/entities/job.py
+++ src/domain/entities/job.py
@@ -1,17 +1,16 @@
-from dataclasses import dataclass, field
 from datetime import datetime
 from typing import List
 from typing import Optional, Callable
+from pydantic import BaseModel, Field
 
 
-@dataclass
-class Job:
-    func: Callable = field(default=None)
-    trigger: str = field(default=None)
-    run_date: datetime = field(default=None)
-    hour: int = field(default=None)
-    minute: int = field(default=None)
-    args: list = field(default=None)
-    job_id: Optional[str] = field(default=None)
-    job_type: Optional[str] = field(default=None)
+class Job(BaseModel):
+    func: Callable = Field(default=None)
+    trigger: str | None = Field(default=None)
+    run_date: datetime | None = Field(default=None)
+    hour: int | None = Field(default=None)
+    minute: int | None = Field(default=None)
+    args: list | None = Field(default=None)
+    job_id: Optional[str] = Field(default=None)
+    job_type: Optional[str] = Field(default=None)
 
