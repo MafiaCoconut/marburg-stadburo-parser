bc9f04cf30c17d414eaf85a9d259009fe722cae9
MafiaCoconut <alex.kach.2000@gmail.com>
2024-08-27 15:08:53 +0200

    изменил название файлов отправляеммых в s3

--- src/application/use_cases/s3_use_case.py
+++ src/application/use_cases/s3_use_case.py
@@ -16,8 +16,8 @@ class S3UseCase:
     @log_decorator
     async def upload_logs(self):
         logs_files = {
-            "stadburo_system_logs": "logs/system_data.log",
-            "stadburo_error_logs": "logs/error_data.log",
+            "stadburo_system_logs.txt": "logs/system_data.log",
+            "stadburo_error_logs.txt": "logs/error_data.log",
         }
         for file_name, file_path in logs_files.items():
             await self.s3_client.upload_file(file_path=file_path, file_name=file_name)
